c5ed7e0b412f2284978e515b349a372b9a970831
fwcd <fwcdmail@gmail.com>
2020-06-20 19:53:12 +0200

    List requirements in install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="lights",
     version="0.1.0",
     packages=["lights"],
+    install_requires=[
+        "phue==1.1"
+    ],
     entry_points={
         "console_scripts": ["lights = lights.__main__:main"]
     }
