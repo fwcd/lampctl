59f726cf39b8229557cd39b4810aaf054984a0ab
fwcd <fwcdmail@gmail.com>
2022-04-03 00:11:37 +0200

    Set up testing infrastructure

---
+++ lights/test/test_colors.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestColors(unittest.TestCase):
+    def test_rgb(self):
+        # TODO
+        self.assertEqual(False, True)
+
+if __name__ == "__main__":
+    unittest.main()
